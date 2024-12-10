1115841aceff777c0608e4be2a0b170337e5bf75
Martina Muñoz <martinaemunoz@gmail.com>
2024-12-10 16:02:11 -0300

    First commit

---
+++ Complementary.java
@@ -0,0 +1,11 @@
+
+public class Complementary {
+    public static void main(String[] args){
+        String mensajeBienvenida = "Bienvenido a Java";
+        int temperaturaActual = 19;
+        boolean datoLogico = true;
+        System.out.println(mensajeBienvenida);
+        System.out.println(temperaturaActual);
+        System.out.println(datoLogico);
+    }
+}
